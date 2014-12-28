3f6a25f7bf6042daeafc78e405313a7218e16eba
John Lopez <admin@tronlogistics.com>
2014-12-28 11:58:49 -0600

    added some error handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
+
 import os
 
 app = Flask(__name__)
@@ -19,6 +20,18 @@ app.register_blueprint(load)
 #app.register_blueprint(carrier)
 #app.register_blueprint(dashboard)
 app.register_blueprint(static)
+
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/tms.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('TMS startup')
 
 # Function to easily find your assets
 # In your template use <link rel=stylesheet href="{{ static('filename') }}">
--- app/views/static.py
+++ app/views/static.py
@@ -87,3 +87,14 @@ def register():
 		return redirect(url_for('.login'))
 	return render_template('static/register.html', form=form, user=g.user)
 
+@app.errorhandler(404)
+def not_found_error(error):
+	app.logger.exception(error)
+	return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+	app.logger.exception(error)
+	db.session.rollback()
+	return render_template('500.html'), 500
+
