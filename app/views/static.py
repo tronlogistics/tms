afbdf02a527563b7e33869aa84c336ba8fea4ba2
John Lopez <admin@tronlogistics.com>
2014-12-29 14:32:24 -0600

    adding flask-principal

--- app/views/static.py
+++ app/views/static.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, render_template, url_for, redirect, request, flash, session, g, current_app
 from flask.ext.login import login_user, logout_user, current_user, login_required
-#from flask.ext.principal import identity_loaded, Principal, Identity, AnonymousIdentity, identity_changed, RoleNeed, UserNeed
+from flask.ext.principal import identity_loaded, Principal, Identity, AnonymousIdentity, identity_changed, RoleNeed, UserNeed
 from app import db, lm, app
 from app.forms import LoginForm, RegisterForm
 from app.models import User, Role
-#from app.permissions import *
+from app.permissions import *
 
 
 
@@ -99,4 +99,51 @@ def internal_error(error):
 	app.logger.exception(error)
 	db.session.rollback()
 	return render_template('500.html', error=error), 500
+
+@identity_loaded.connect_via(app)
+def on_identity_loaded(sender, identity):
+	# Set the identity user object
+	identity.user = current_user
+
+	# Add the UserNeed to the identity
+	if hasattr(current_user, 'id'):
+		identity.provides.add(UserNeed(current_user.id))
+
+	# Assuming the User model has a list of roles, update the
+	# identity with the roles that the user provides
+	if hasattr(current_user, 'roles'):
+		for role in current_user.roles:
+			identity.provides.add(RoleNeed(role.name))
+
+	# Assuming the User model has a list of posts the user
+	# has authored, add the needs to the identity
+	if hasattr(current_user, 'brokered_loads'):
+		for load in current_user.brokered_loads:
+			identity.provides.add(EditLoadNeed(unicode(load.id)))
+			identity.provides.add(DeleteLoadNeed(unicode(load.id)))
+			identity.provides.add(ViewLoadNeed(unicode(load.id)))
+			identity.provides.add(AssignLoadNeed(unicode(load.id)))
+			if load.assigned_driver is not None:
+				identity.provides.add(ViewDriverNeed(unicode(load.assigned_driver.id)))
+
+	if hasattr(current_user, 'assigned_loads'):
+		for load in current_user.assigned_loads:
+			identity.provides.add(ViewLoadNeed(unicode(load.id)))
+			identity.provides.add(AssignLoadNeed(unicode(load.id)))
+
+	if hasattr(current_user, 'fleet'):
+		for truck in current_user.fleet.trucks:
+			identity.provides.add(EditTruckNeed(unicode(truck.id)))
+			identity.provides.add(DeleteTruckNeed(unicode(truck.id)))
+			identity.provides.add(ViewTruckNeed(unicode(truck.id)))
+
+		for driver in current_user.fleet.drivers:
+			identity.provides.add(EditDriverNeed(unicode(driver.id)))
+			identity.provides.add(DeleteDriverNeed(unicode(driver.id)))
+			identity.provides.add(ViewDriverNeed(unicode(driver.id)))
+
+	if hasattr(current_user, 'offered_bids'):
+		for bid in current_user.offered_bids:
+			identity.provides.add(ViewLoadNeed(unicode(bid.load.id)))
+			identity.provides.add(AssignLoadNeed(unicode(bid.load.id)))
 
