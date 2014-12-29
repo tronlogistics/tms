edd86c1bef418427c872dd547a8609ab846cf8ee
John Lopez <admin@tronlogistics.com>
2014-12-29 00:48:41 -0600

    db migrate

--- app/models.py
+++ app/models.py
@@ -9,10 +9,10 @@ from sqlalchemy.orm import scoped_session, sessionmaker, backref, relationship
 #	db.Column("right_user_id", db.Integer, db.ForeignKey("User.id"), primary_key=True)
 #)
 
-#assigned_users = db.Table('assigned_users', db.metadata,
-#	Column('user_id', Integer, ForeignKey('User.id')),
-#	Column('load_id', Integer, ForeignKey('Load.id'))
-#)
+assigned_users = db.Table('assigned_users', db.metadata,
+	Column('user_id', Integer, ForeignKey('User.id')),
+	Column('load_id', Integer, ForeignKey('Load.id'))
+)
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -44,7 +44,7 @@ class User(db.Model):
 		self.email = email
 		self.company_name = company_name
 		self.set_password(password)
-		#self.fleet = Fleet()
+		self.fleet = Fleet()
 
 	def is_carrier(self):
 		return len(filter((lambda role: role.name == 'carrier'), self.roles)) == 1
