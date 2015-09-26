6d7486dd5101a6b835da78b851bc35f27f380c0c
John Lopez <johnny.lopez617@gmail.com>
2015-09-26 12:40:54 -0500

    Admin update

--- app/__init__.py
+++ app/__init__.py
@@ -101,5 +101,5 @@ from models import User, Role, Load
 admin = Admin(app, name='Tron Logistics', index_view=MyAdminIndexView())
 admin.add_view(MyModelView(User, db.session))
 admin.add_view(MyModelView(Role, db.session))
-admin.add_view(ModelView(Load, db.session))
+admin.add_view(MyModelView(Load, db.session))
 
