164b9d9502d637eb5cac53f20ae67b8d5c3655b8
John Lopez <johnny.lopez617@gmail.com>
2015-11-15 15:35:33 -0600

    test

--- app/views/api.py
+++ app/views/api.py
@@ -173,18 +173,15 @@ class LocationAPI(Resource):
 
     def put(self, load_id, location_id):
         print("-----PUT------")
-        print (self)
         load = [load for load in g.user.company.loads if load.id == load_id]
-        print (load[0].id)
         location = None
         for cur_location in load[0].lane.locations:
             if cur_location.id == location_id:
                 location = cur_location
-        print (location.id)
         args = self.reqparse.parse_args()
         for k, v in args.iteritems():
             if v != None:
-                print ("K %s - V %s" % (k, v))
+                print("test")
                 #task[k] = v
         return jsonify( { 'task': 'task' } )
 
