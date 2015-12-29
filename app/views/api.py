16dbb145299c14741bee68fb07d4d87bd85a0d98
John Lopez <=>
2015-12-29 13:12:03 -0600

    updates

--- app/views/api.py
+++ app/views/api.py
@@ -225,7 +225,7 @@ class LongLatAPI(Resource):
         super(LongLatAPI, self).__init__()
 
     def post(self):
-        print json.dumps(request)
+        print request
         #for driver in g.user.driver_instances:
         #    print "found instance"
         #    if driver.truck is not None:
